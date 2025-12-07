f72d4f5fce1ccdd577ca84567382e0cbea468bb0
Leandro Pozo <jordanspozo@gmail.com>
2025-12-07 12:24:40 -0500

    feat(audioplayers): integrar el plugin de audioplayers y agregar sonido a la funcionalidad del boton favoritos, cuando se lo pulsa genera un sonido.

--- lib/presentation/widgets/shared/custom_bottom_favorites.dart
+++ lib/presentation/widgets/shared/custom_bottom_favorites.dart
@@ -5,52 +5,89 @@ import 'package:movies_app/domain/entities/movie.dart';
 import 'package:movies_app/presentation/providers/storage/favorite_movies_provider.dart';
 import 'package:movies_app/presentation/providers/storage/is_favorite_movie_provider.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-class CustomBottomFavorites extends ConsumerWidget {
+class CustomBottomFavorites extends ConsumerStatefulWidget {
   final Movie movie;
   const CustomBottomFavorites({super.key, required this.movie});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isFavoriteFuture = ref.watch(isFavoriteMovieProvider(movie.id));
-    final size = MediaQuery.of(context).size;
+  ConsumerState<CustomBottomFavorites> createState() => _CustomBottomFavoritesState();
+}
 
-    //final colors = Theme.of(context).colorScheme;
+class _CustomBottomFavoritesState extends ConsumerState<CustomBottomFavorites> {
+  
+  //! PARA EL USO DE SONIDSO ES NECESARIO EL PLUGIN AUDIOPLAYERS
+  //! SE REQUIERE UN STATEFUL O CONSUMERFUL, ADEMAS DE MODIFICAR EL ANDROIDMANIFEST
+  //! UNA FUNCION FUTURA Y TERMINAR EL VALOR CON EL DISPOSE 
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  Future<void> reproducirSonido() async{
+    await _audioPlayer.setVolume(0.15);//* REGULAR EL VOLUMEN 1 MAXIMO
+    await _audioPlayer.play(AssetSource('sounds/bambu_1.mp3'));
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+  
+  //* CONSTRUCCION DEL SNACKBAR SI AGREGA O QUITA DE FAVORITOS
+  void snackBar(BuildContext context, String message){
+    ScaffoldMessenger.of(context).clearSnackBars();//* Cierra el anterior
+    final snackBar = SnackBar(//* crea el nuevo
+      content: Text(message),
+      behavior: SnackBarBehavior.floating,//* efecto de estar flotando en la pantalla
+      backgroundColor: const Color.fromARGB(255, 198, 197, 197),
+      // action: SnackBarAction(
+      //   label: 'Ok', 
+      //   onPressed: (){}
+      // ),
+
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(10)
+      ),
+      duration: Duration(seconds: 1),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);//* muestra el nuevo
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isFavoriteFuture = ref.watch(isFavoriteMovieProvider(widget.movie.id));
+    final size = MediaQuery.of(context).size;
+    final valueFavorite = isFavoriteFuture.value ?? false;
+    //final colors = Theme.of(context).colorScheme;    
+    
     return Padding(
       padding: EdgeInsetsGeometry.only(top: 15, bottom: 5),
       child: InkWell(
         onTap: () async{
-          //! PARTE DEL SNACKBAR PARA QUE ACTUALICE RAPIDO SI EL USUARIO QUITA Y PONE RAPIDO
-          final messenger = ScaffoldMessenger.of(context);
-          messenger.hideCurrentSnackBar();
 
           //! ACTUALIZAR LA DATABASE
-          await ref.read(favoriteMoviesProvider.notifier).toggleFavoriteMovie(movie);
-          ref.invalidate(isFavoriteMovieProvider(movie.id));
-        
+          await ref.read(favoriteMoviesProvider.notifier).toggleFavoriteMovie(widget.movie);
+          ref.invalidate(isFavoriteMovieProvider(widget.movie.id));
           final isFav = isFavoriteFuture.value ?? false;//* Obtenemos el valor
+          
           if(isFav == true){//* Significa que es un favorito, si lo pulsa de debe mostrar mensaje de se quito
-            messenger.showSnackBar(
-              SnackBar(
-                content: Text('Se quito de tus favoritos'),
-                duration: Duration(seconds: 1),
-              )
-            );
+            // ignore: use_build_context_synchronously
+            snackBar(context, 'Se quito de tus favoritos');
           }else{
-            messenger.showSnackBar(
-              SnackBar(
-               content: Text('Se agrego a tus favoritos'),
-               duration: Duration(seconds: 1),
-              )
-            );
+            // ignore: use_build_context_synchronously
+            snackBar(context, 'Se agrego a tus favoritos');
           }
+
+          reproducirSonido();
         },
         child: ZoomIn(
           child: Container(
             
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(5),
-              color: Colors.transparent,
+              color: valueFavorite ? 
+              const Color.fromARGB(255, 105, 106, 107)
+              :
+              Colors.transparent,
               border: Border.all(
                 width: 2, 
                 color: Colors.white
@@ -60,7 +97,7 @@ class CustomBottomFavorites extends ConsumerWidget {
             height: size.height * 0.05,
             child: isFavoriteFuture.when(
               data: (isFavorite) => isFavorite == true ?
-              Icon(Icons.check, color: const Color.fromARGB(255, 60, 193, 182), size: 25,)
+              Icon(Icons.check, size: 27,)
               :
               Icon(Icons.add), 
               error: (_, __) => throw Exception("Error al cargar el estado de favoritos"), 
